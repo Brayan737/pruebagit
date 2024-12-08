2711a9233901f11c3775e4d7cb33cc60330799c3
Brayan373 <brayanzabala6@gmail.com>
2024-12-07 22:33:47 -0300

    Actualizacion Final.

--- Aprendiendo/Aprendiendo 4/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const readlineSync = require('readline-sync');
-// Cambiar "prompt" a "ask"
-const ask = (question) => {
-    return readlineSync.question(question);
-};
-const saludo = ask("¿Cuál es tu nombre? ");
-console.log(`Hola, ${saludo}!`);
--- Aprendiendo/Aprendiendo 4/src/index.ts
+++
@@ -1,9 +0,0 @@
-const readlineSync = require('readline-sync');
-
-// Cambiar "prompt" a "ask"
-const ask = (question: string): string => {
-    return readlineSync.question(question);
-};
-
-const saludo = ask("¿Cuál es tu nombre? ");
-console.log(`Hola, ${saludo}!`);
